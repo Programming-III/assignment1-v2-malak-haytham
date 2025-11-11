89fff499e02d166e01e121ede839973b1505f7c0
malak-haytham <malak.haytham@student.giu-uni.de>
2025-11-11 09:59:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,25 @@ using namespace std;
 // ==================== Person Class Implementation =========================
 
 
+int main() {
 
+    cout<<"Course: "<< CourseName<<endl;
+    int maxStudents;
+    cout<<"Max Students: "<< maxStudents<<endl;
+int name;
+int id;
+    cout<<"Currently Enrolled: "<<name <<id<<endl;
+    cout<<"Instructor Info: "<<endl;
+    cout<<"Name: "<<endl;
+    cout<<"Department: "<<endl;
+    cout<<"Expereince years: "<<endl;
+    cout<<"Student Info: "<<endl;
+    cout<<"Name: "<<endl;
+    cout<<"Year: "<<endl;
+    cout<<"Major: "<<endl;
+
+
+}
 
 
 
