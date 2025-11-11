e67752992f07d25a43278d4ad1cb09456f5ab810
malak-haytham <malak.haytham@student.giu-uni.de>
2025-11-11 10:02:24 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -8,77 +8,6 @@
 //#include <student.h>
 using namespace std;
 
-class Person {
-#include<iostream>
-private:
-    string name;
-    int id;
-public:
-    Person(string name, int id) {
-        this->name = name;
-        this->id = id;
-    }
-    Person() {
-        name="";
-        id=0;
-    }
-
-#include<ostream>
-    void dispaly() {
-        std::cout<<name<<std::endl;
-        cout<<id<<endl;
-
-    }
-};
-#endif
-
-    class Student: public Person {
-        int yearLevel;
-        string major;
-        public:
-
-        Student(int yearLevel, string major) {
-            this-> yearLevel = yearLevel;
-            this-> major = major;
-        }
-        Student() {
-            yearLevel=0;
-            major="";
-        }
-        string getMajor() {
-            return major;
-        }
-
-        void dispaly() {
-            cout<<major<<endl;
-
-        }
-
-    };
-
-    class Instructor: public Person {
-        private:
-        string department;
-        int experienceYears;
-        public:
-        Instructor(string department, int experienceYears) {
-            this-> department = department;
-            this-> experienceYears = experienceYears;
-        }
-        Instructor() {
-            experienceYears=0;
-           department="";
-        }
-        void dispaly() {
-            cout<<department<<endl;
-            cout<<experienceYears<<endl;
-        }
-
-
-
-    };
-
-
 class Course {
     private:
     string courseCode;
@@ -122,25 +51,4 @@ class Course {
 
 };
 
-int CourseName;
-
-int main() {
-
-    cout<<"Course: "<< CourseName<<endl;
-    int maxStudents;
-    cout<<"Max Students: "<< maxStudents<<endl;
-int name;
-int id;
-    cout<<"Currently Enrolled: "<<name <<id<<endl;
-    cout<<"Instructor Info: "<<endl;
-    cout<<"Name: "<<endl;
-    cout<<"Department: "<<endl;
-    cout<<"Expereince years: "<<endl;
-    cout<<"Student Info: "<<endl;
-    cout<<"Name: "<<endl;
-    cout<<"Year: "<<endl;
-    cout<<"Major: "<<endl;
-
-
-}
 
